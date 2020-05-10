d0cb6e08c7946a5d4e1552d38a9dc09b658a6210
AlekseyK <aleksey.kabishau@gmail.com>
2020-05-10 18:43:52 -0400

    feat: set navigation bar appearance

--- ToDoList/Support/AppDelegate.swift
+++ ToDoList/Support/AppDelegate.swift
@@ -11,10 +11,10 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		UINavigationBar.appearance().isTranslucent = false
+		UINavigationBar.appearance().barTintColor = UIColor(red: 0.33, green: 0.6, blue: 0.75, alpha: 1.0)
+		
 		return true
 	}
 
